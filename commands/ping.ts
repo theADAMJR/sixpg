20d3a4421524a4b1041525db3dee8a89a9891cf9
theADAMJR <adamjf123@gmail.com>
2020-04-27 15:45:27 +0100

    refactor cooldowns - fully tested

--- commands/ping.ts
+++ commands/ping.ts
@@ -4,7 +4,7 @@ import { ModuleString } from '../models/guild';
 export default class PingCommand implements Command {
     name = 'ping';
     summary = 'Probably the best command ever created.';
-    cooldown = 5;
+    cooldown = 3;
     module: ModuleString = 'General';
     
     execute = (ctx: CommandContext) => ctx.channel.send(`🏓 Pong! \`${ctx.bot.ws.ping}ms\``);
